86c51e3244a319f3098cb40dec959397bf9521ca
SakaCorpoRat <ajwarner@albany.edu>
2025-04-09 17:18:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 echo "[ Cassette Linux Setup ]"
 echo "Installing dependencies..."
 
 sudo apt update && sudo apt install -y \
   i3 kitty conky polybar neofetch feh \
-  fonts-terminus dmenu
+  mpv xlockmore-gl fonts-terminus dmenu
 
 mkdir -p ~/.config/{i3,kitty,polybar,conky,neofetch} ~/.local/bin
 
@@ -22,13 +24,13 @@ backup_and_copy() {
 }
 
 echo "Copying dotfiles..."
-backup_and_copy i3/config ~/.config/i3/config
-backup_and_copy kitty/kitty.conf ~/.config/kitty/kitty.conf
-backup_and_copy polybar/config.ini ~/.config/polybar/config.ini
-backup_and_copy conky/conky.conf ~/.config/conky/conky.conf
-backup_and_copy neofetch/config.conf ~/.config/neofetch/config.conf
+backup_and_copy "$SCRIPT_DIR/i3/config" ~/.config/i3/config
+backup_and_copy "$SCRIPT_DIR/kitty/kitty.conf" ~/.config/kitty/kitty.conf
+backup_and_copy "$SCRIPT_DIR/polybar/config.ini" ~/.config/polybar/config.ini
+backup_and_copy "$SCRIPT_DIR/conky/conky.conf" ~/.config/conky/conky.conf
+backup_and_copy "$SCRIPT_DIR/neofetch/config.conf" ~/.config/neofetch/config.conf
 
-cp -r scripts/* ~/.local/bin/
+cp -r "$SCRIPT_DIR/scripts/"* ~/.local/bin/
 chmod +x ~/.local/bin/*
 
 echo "[ Done ] Welcome to the retrofuture."
